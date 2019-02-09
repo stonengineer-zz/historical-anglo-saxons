85216620b71c215cd52ea2b4e0b8d645bec2e6ad
stonengineer <josephwstone@gmail.com>
2019-02-08 19:49:15 -0600

    made Æthelred not as terrible, but more prone to dying. gave Alfred 2 combat traits, but demoted his training level. added deploy.bat, will explain in the README

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+@echo off
+@echo name="True Anglo Saxons" > historical_anglo_saxons.mod.mod
+@echo.
+@echo archive="mod/historical-anglo-saxons.zip" >> historical_anglo_saxons.mod.mod
+move historical_anglo_saxons.mod.mod ../
+pause
